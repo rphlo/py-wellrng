0bcad016e0953e2426ca5d52023b4035cc838d33
Raphaël Stefanini <raphael.stefanini@payiq.net>
2017-01-26 12:10:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from setuptools import setup
 setup(
   name = 'wellrng',
   packages = ['wellrng'],
-  version = '0.2',
+  version = '0.3',
   description = 'A replacment for the default random lib using WELL1024a RNG',
   author = 'Raphael Stefanini',
   author_email = 'rphl@rphl.net',
   url = 'https://github.com/rphlo/py-wellrng',
-  download_url = 'https://github.com/rphlo/py-wellrng/tarball/0.2',
+  download_url = 'https://github.com/rphlo/py-wellrng/tarball/0.3',
   keywords = ['random', 'well1024a', 'PRNG', 'RNG'],
-  classifiers = [],
+  classifiers = [
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+  ],
 )
